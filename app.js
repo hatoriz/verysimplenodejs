7007fca7371cc0ce5f012e93149568a284c3dcec
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2016-10-08 22:21:23 +0700

    [NODE] Test Jenkins webhook

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req,res) {
-  res.send("Hello World, this is Hatoriz");
+  res.send("Hello World from Hatoriz");
 });
 
 app.get("/greeting", function(req,res){
