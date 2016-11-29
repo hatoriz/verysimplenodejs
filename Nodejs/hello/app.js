2ea16742aa3269e9acac87717ee89035fd4addeb
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2016-11-29 09:31:08 +0700

    [GIT] - Reorg folder

---
+++ Nodejs/hello/app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.get("/",function(req,res) {
+  res.send("Hello World, from Hatoriz");
+});
+
+app.get("/greeting", function(req,res){
+  res.send("greeting has been returned !");
+});
+
+app.listen("8899");
